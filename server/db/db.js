37a552c5b03c244f21d8c505aba95d8ab59ae79d
Phil He <101652706+madorominemu@users.noreply.github.com>
2022-09-27 03:23:29 +0800

    Delete server directory

--- server/db/db.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const $mysql = require('mysql');
-const sql = require('./mysql.js')
-const $sql = $mysql.createConnection(sql.mysql)
-
-$sql.connect();
-module.exports = $sql;
